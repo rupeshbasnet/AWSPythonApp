54df18910ff7afd064f21eb2e43e1efa821a4302
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-07 00:35:53 -0500

    Added the hellow world

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+application = Flask(__name__)
+@application.route('/')
+
+def hello():
+ return "Hello World"
+# run the app.
+if __name__ == "__main__":
+ application.debug = True
+ application.run()
